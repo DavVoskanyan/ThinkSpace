663dec5c4d0191d889a5b7d1c4191128d1dfa8df
DavVoskanyan <81379810+DavVoskanyan@users.noreply.github.com>
2022-12-28 22:53:45 +0400

    Add files via upload

---
+++ FastNotes/scripts/fastNotes.js
@@ -0,0 +1,63 @@
+import NotesWorker from "/assets/globalScripts/NotesWorker.js";
+import NoteConverter from "/assets/globalScripts/NoteConverter.js";
+import ModalController from "/assets/globalScripts/ModalController.js";
+import IndexedDataBaseWorker from "/assets/globalScripts/IndexedDataBaseWorker.js";
+import WindowAlerter from "/assets/globalScripts/WindowAlerter.js";
+
+//////////////////////////   A D D   N O T E S   F R O M   S T O R A G E   /////////////////////////////
+const notesContainer = document.querySelector('.notesContainer');
+const notesFromStorage = new NotesWorker();
+const noteConverter = new NoteConverter();
+
+notesFromStorage.getAllNotes().forEach(noteObject => {
+  notesContainer.appendChild(noteConverter.objectToElement(noteObject));
+})
+
+
+//////////////////////////   N O T E - L I S T   V I E W   C H A N G E   B U T T O N S   ///////////////////////////////
+const gridButton = document.querySelector('.gridView');
+const barView = document.querySelector('.barView');
+const notesList = document.querySelector('.notesContainer');
+
+gridButton.addEventListener("click", () => {
+    gridButton.classList.add('selected');
+    barView.classList.remove('selected');
+
+    notesList.classList.remove('notesList');
+    notesList.classList.add('notesGrid');
+});
+barView.addEventListener("click", () => {
+    barView.classList.add('selected');
+    gridButton.classList.remove('selected');
+
+    notesList.classList.remove('notesGrid');
+    notesList.classList.add('notesList');
+});
+
+
+
+
+/////////////////////////   A D D   N E W   N O T E   M O D A L   //////////////////////////
+const newNoteModalController = new ModalController('addNoteModal');
+const addNewNoteButton = document.querySelector('button.addNewThought');
+addNewNoteButton.addEventListener('click', () => newNoteModalController.openModal() );
+
+
+
+const submitButtonInModal = document.querySelector('.addNewNote');
+
+submitButtonInModal.addEventListener('click', (e) => {
+    e.preventDefault();
+    notesFromStorage.addSingleNote(noteConverter.objectFromInputs(document.querySelector('#addNoteModal form')))
+    newNoteModalController.closeModal();
+})
+
+
+
+
+let idb = new IndexedDataBaseWorker('ThinkSpace', 1);
+console.log(idb.getAllObjects('notesStore'));
+
+
+const alerter = new WindowAlerter();
+alerter.alertDivConstructor("success", "This is a successful alert :)")
---
+++ assets/globalScripts/IndexedDataBaseWorker.js
@@ -0,0 +1,69 @@
+
+// const openRequest = indexedDB.open('ThinkSpace', 2);
+
+
+class IndexedDataBaseWorker {
+  idbName;
+  idbVersion;
+  constructor(idbName, idbVersion) {
+    this.idbName = idbName;
+    this.idbVersion = idbVersion;
+  }
+
+  setNewObject(storeName, needToSetObject) {
+    const openIdbRequest = indexedDB.open(this.idbName, this.idbVersion);
+
+    openIdbRequest.onerror = () => {
+      console.error('Can not connect to IndexedDB.');
+    }
+    openIdbRequest.onupgradeneeded = () => {
+      console.log('Updating IndexedDB');
+      openIdbRequest.result.createObjectStore(storeName, { keyPath: 'id', autoIncrement: true });
+    }
+    openIdbRequest.onsuccess = () => {
+      console.log('Successfully connected to IndexedDB');
+
+      const writeTransaction = openIdbRequest.result.transaction(storeName, 'readwrite');
+
+      writeTransaction.onerror = () => console.error('Con not make transaction');
+
+      const currentObjectStore = writeTransaction.objectStore(storeName);
+      const writeRequest = currentObjectStore.add(needToSetObject);
+    }
+  }
+
+  getAllObjects(storeName) {
+    const openIdbRequest = indexedDB.open(this.idbName, this.idbVersion);
+    let objectsArray = [];
+
+    openIdbRequest.onerror = () => {
+      console.error('Can not connect to IndexedDB.');
+    }
+    openIdbRequest.onupgradeneeded = () => {
+      console.log('Updating IndexedDB');
+      openIdbRequest.result.createObjectStore(storeName, { keyPath: 'id', autoIncrement: true });
+    }
+    openIdbRequest.onsuccess = () => {
+      console.log('Successfully connected to IndexedDB');
+
+      const writeTransaction = openIdbRequest.result.transaction(storeName, 'readonly');
+
+      writeTransaction.onerror = () => console.error('Con not make transaction');
+
+      const currentObjectStore = writeTransaction.objectStore(storeName);
+      const readRequest = currentObjectStore.getAll();
+
+      readRequest.onsuccess = () => {
+        readRequest.result.forEach(object => {
+          objectsArray.push(object);
+        })
+      }
+    }
+
+    return objectsArray;
+  }
+
+
+}
+
+export default IndexedDataBaseWorker;

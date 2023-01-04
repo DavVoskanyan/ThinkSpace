842cde6578f431684e5265da0929e8bcdd0fb9c0
DavVoskanyan <81379810+DavVoskanyan@users.noreply.github.com>
2023-01-04 18:02:09 +0400

    Add files via upload

--- FastNotes/scripts/fastNotes.js
+++ FastNotes/scripts/fastNotes.js
@@ -59,5 +59,6 @@ let idb = new IndexedDataBaseWorker('ThinkSpace', 1);
 console.log(idb.getAllObjects('notesStore'));
 
 
-const alerter = new WindowAlerter();
-alerter.alertDivConstructor("success", "This is a successful alert :)")
+const alerter = new WindowAlerter(document.querySelector('#rightSideBar'));
+alerter.alertDivConstructor("error", "This is a successful alert :)");
+setTimeout(() => alerter.alertDivConstructor("success", "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry."), 2500)
--- assets/globalScripts/WindowAlerter.js
+++ assets/globalScripts/WindowAlerter.js
@@ -1,37 +1,51 @@
 class WindowAlerter {
-  ERROR_SVG_FILE_NAME = 'xMark';
-  SUCCESS_SVG_FLE_NAME = 'checkMark';
+  static NOTIFICATIONS_CONTAINER;
+  #ERROR_SVG_FILE_NAME = 'xMark';
+  #SUCCESS_SVG_FLE_NAME = 'checkMark';
 
+  constructor(notificationContainerParentElement) {
+    WindowAlerter.NOTIFICATIONS_CONTAINER = WindowAlerter.NOTIFICATIONS_CONTAINER ?? WindowAlerter.#createNotificationContainer(notificationContainerParentElement);
+  }
+  static #createNotificationContainer(notificationContainerParentElement) {
+    const container = document.createElement('div');
+    container.classList.add('notificationsContainer');
+    container.innerHTML = "<h2 class='notificationsTitle'>Notifications</h2>"
+
+
+
+    if(notificationContainerParentElement) {
+      notificationContainerParentElement.appendChild(container);
+    }
+    else { console.error(`WindowAlerterError: Parent Element is ${notificationContainerParentElement}`) }
+    return container;
+  }
 
   alertDivConstructor(alertType, alertText) {
     const alertDiv = document.createElement('div');
     alertDiv.classList.add('alertWindow');
 
-    alertDiv.appendChild(this.createWindowIcon(alertType));
-    alertDiv.appendChild(this.createWindowText(alertText));
+    alertDiv.appendChild(this.#createWindowIcon(alertType));
+    alertDiv.appendChild(this.#createWindowText(alertText));
 
-    document.body.appendChild(alertDiv);
+    WindowAlerter.NOTIFICATIONS_CONTAINER.appendChild(alertDiv);
 
-    let disappearTimeOut = setTimeout(() => {this.removeWindow(alertDiv)}, 4000);
-
-    alertDiv.addEventListener('mouseover', () => clearTimeout(disappearTimeOut) );
-    alertDiv.addEventListener('mouseout', () => disappearTimeOut = setTimeout(() => {this.removeWindow(alertDiv)}, 2000) );
+    setTimeout(() => {this.#moveToList(alertDiv)}, 4000);
   }
 
-  getFileNameByAlertType(alertType) {
-    return alertType.trim() === 'success' ? this.SUCCESS_SVG_FLE_NAME : this.ERROR_SVG_FILE_NAME;
+  #getFileNameByAlertType(alertType) {
+    return alertType.trim().toLowerCase() === 'success' ? this.#SUCCESS_SVG_FLE_NAME : this.#ERROR_SVG_FILE_NAME;
   }
 
-  createWindowIcon(alertType) {
+  #createWindowIcon(alertType) {
     const windowIcon = document.createElement('img');
-    windowIcon.src = `/assets/icons/${this.getFileNameByAlertType(alertType)}.svg`;
-    windowIcon.alt = this.getFileNameByAlertType(alertType);
-    windowIcon.classList.add('alertIcon');
+    windowIcon.src = `/assets/icons/${this.#getFileNameByAlertType(alertType)}.svg`;
+    windowIcon.alt = this.#getFileNameByAlertType(alertType);
+    windowIcon.classList.add('alertIcon', alertType.trim().toLowerCase());
 
     return windowIcon;
   }
 
-  createWindowText(alertText) {
+  #createWindowText(alertText) {
     const windowText = document.createElement('span');
     windowText.innerText = alertText;
     windowText.classList.add('windowText');
@@ -39,9 +53,13 @@ class WindowAlerter {
     return windowText;
   }
 
-  removeWindow(alertWindow) {
+  #moveToList(alertWindow) {
     alertWindow.classList.add('removed');
-    setTimeout(() => alertWindow.remove(), 200);
+
+    setTimeout(() => {
+      alertWindow.classList.remove('removed');
+      alertWindow.classList.add('addedToList');
+    }, 150)
   }
 }
 

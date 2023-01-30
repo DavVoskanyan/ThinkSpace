06a29b4fd906dc8a1c23135db37ccc255d188c1d
David Voskanyan <voskanyandavid001001@gmail.com>
2023-01-30 10:03:42 +0400

    Some fixes, added .gitignore file and removed from git TestModules.js

--- FastNotes/scripts/fastNotes.js
+++ FastNotes/scripts/fastNotes.js
@@ -6,8 +6,12 @@ import NotesView from "/assets/globalScripts/NotesView.js";
 
 //////////////////////////   A D D   N O T E S   F R O M   I N D E X E D - D B   /////////////////////////////
 const notesContainer = new NotesView(document.querySelector("#mainContent"), true);
-notesContainer.addNewNote('Hello World', "Lorem Ipsum dolor sit amet", new Date());
+notesContainer.addNewNote('Hello World 1', "Lorem Ipsum dolor sit amet", new Date());
 notesContainer.addNewNote('Hello World 2', "Lorem Ipsum dolor sit sequel", new Date());
+notesContainer.addNewNote('Hello World 3', "Lorem Ipsum dolor sit amet", new Date());
+notesContainer.addNewNote('Hello World 4', "Lorem Ipsum dolor sit sequel", new Date());
+notesContainer.addNewNote('Hello World 5', "Lorem Ipsum dolor sit amet", new Date());
+notesContainer.addNewNote('Hello World 6', "Lorem Ipsum dolor sit sequel", new Date());
 
 
 

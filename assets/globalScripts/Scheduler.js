4de3a2854b8ea5509011959be9ad2cdfc7d0231f
David Voskanyan <voskanyandavid001001@gmail.com>
2023-02-10 01:12:59 +0400

    Scheduler work with date is finished

--- assets/globalScripts/Scheduler.js
+++ assets/globalScripts/Scheduler.js
@@ -1,8 +1,14 @@
 class Scheduler {
     #PARENT_NODE;
-
-    #DAYS_ARRAY = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
+    #CURRENT_DATE_CONTAINER;
+    #DAYS_ARRAY = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+    #MONTHS_ARRAY = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
     #currentShowingDate;
+    #CALENDARS_CONTAINER
+
+    #previousMonth;
+    #currentMonth;
+    #nextMonth;
 
     constructor(parentNode) {
         this.#PARENT_NODE = parentNode;
@@ -27,12 +33,60 @@ class Scheduler {
 
         const dateLine = document.createElement('span');
         dateLine.classList.add('dateLine');
+        dateLine.innerText = `${this.#MONTHS_ARRAY[this.#currentShowingDate.getMonth()]}, ${this.#currentShowingDate.getFullYear()}`;
+        this.#CURRENT_DATE_CONTAINER = dateLine;
 
         const previousMonthButton = document.createElement('button');
         previousMonthButton.classList.add('previousMonthButton');
+        previousMonthButton.addEventListener('click', () => {
+            this.#currentShowingDate = new Date(
+                this.#currentShowingDate.getFullYear(),
+                this.#currentShowingDate.getMonth() - 1,
+                1);
+            this.#nextMonth.remove();
+
+            this.#currentMonth.classList.remove('currentShowingMonth');
+            this.#currentMonth.classList.add('nextBeShownMonth');
+            this.#nextMonth = this.#currentMonth;
+
+            this.#previousMonth.classList.remove('previousShownMonth');
+            this.#previousMonth.classList.add('currentShowingMonth');
+            this.#currentMonth = this.#previousMonth;
+
+            this.#previousMonth = this.#createCalendarPage(
+                new Date( this.#currentShowingDate.getFullYear(), this.#currentShowingDate.getMonth() - 1, 1 ),
+                'previousShownMonth'
+            );
+            this.#CALENDARS_CONTAINER.insertBefore(this.#previousMonth, this.#currentMonth);
+            this.#CURRENT_DATE_CONTAINER.innerText = `${this.#MONTHS_ARRAY[this.#currentShowingDate.getMonth()]}, ${this.#currentShowingDate.getFullYear()}`;
+        })
+
 
         const nextMonthButton = document.createElement('button');
         nextMonthButton.classList.add('nextMonthButton');
+        nextMonthButton.addEventListener('click', () => {
+            this.#currentShowingDate = new Date(
+                this.#currentShowingDate.getFullYear(),
+                this.#currentShowingDate.getMonth() + 1,
+                1);
+
+            this.#previousMonth.remove();
+
+            this.#currentMonth.classList.remove('currentShowingMonth');
+            this.#currentMonth.classList.add('previousShownMonth');
+            this.#previousMonth = this.#currentMonth;
+
+            this.#nextMonth.classList.remove('nextBeShownMonth');
+            this.#nextMonth.classList.add('currentShowingMonth');
+            this.#currentMonth = this.#nextMonth;
+
+            this.#nextMonth = this.#createCalendarPage(
+                new Date( this.#currentShowingDate.getFullYear(), this.#currentShowingDate.getMonth() + 1, 1 ),
+                'nextBeShownMonth'
+            );
+            this.#CALENDARS_CONTAINER.appendChild(this.#nextMonth);
+            this.#CURRENT_DATE_CONTAINER.innerText = `${this.#MONTHS_ARRAY[this.#currentShowingDate.getMonth()]}, ${this.#currentShowingDate.getFullYear()}`;
+        })
 
         controlLine.appendChild(dateLine);
         controlLine.appendChild(previousMonthButton);
@@ -45,31 +99,49 @@ class Scheduler {
         const calendar = document.createElement('div');
         calendar.classList.add('calendar');
 
+        const daysLine = document.createElement('div');
+        daysLine.classList.add('daysLine');
+
         for(let i = 0; i < 7; i++) {
             let dayDiv = document.createElement('div');
             dayDiv.classList.add('dayCell');
             dayDiv.innerText = this.#DAYS_ARRAY[i];
 
-            calendar.appendChild(dayDiv);
+            daysLine.appendChild(dayDiv);
         }
+
+        calendar.appendChild(daysLine);
 
         const calendarsContainer = document.createElement('div');
         calendarsContainer.classList.add('calendarsContainer');
+        this.#CALENDARS_CONTAINER = calendarsContainer;
 
-        calendarsContainer.appendChild( this.#createCalendarPage( new Date( this.#currentShowingDate.getFullYear(), this.#currentShowingDate.getMonth() - 1, 1 ) ) );
-        calendarsContainer.appendChild( this.#createCalendarPage( this.#currentShowingDate));
-        calendarsContainer.appendChild( this.#createCalendarPage( new Date( this.#currentShowingDate.getFullYear(), this.#currentShowingDate.getMonth() + 1, 1 ) ) );
+        this.#previousMonth = this.#createCalendarPage(
+            new Date( this.#currentShowingDate.getFullYear(), this.#currentShowingDate.getMonth() - 1, 1 ),
+            'previousShownMonth'
+        );
+        this.#currentMonth = this.#createCalendarPage(
+            this.#currentShowingDate,
+            'currentShowingMonth'
+        );
+        this.#nextMonth = this.#createCalendarPage(
+            new Date( this.#currentShowingDate.getFullYear(), this.#currentShowingDate.getMonth() + 1, 1 ),
+            'nextBeShownMonth'
+        );
+
+        calendarsContainer.appendChild( this.#previousMonth );
+        calendarsContainer.appendChild( this.#currentMonth );
+        calendarsContainer.appendChild( this.#nextMonth );
 
         calendar.appendChild(calendarsContainer);
 
         return calendar;
     }
 
-    #createCalendarPage(dateObject) {
+    #createCalendarPage(dateObject, startCssClass) {
         const calendarPage = document.createElement('div');
-        calendarPage.classList.add('calendarList');
-
-        for(let i = 0; i <= new Date(dateObject.getFullYear(), dateObject.getMonth(), 0).getDay(); i++) {
+        calendarPage.classList.add('calendarList', startCssClass);
+        for(let i = 0; i < new Date(dateObject.getFullYear(), dateObject.getMonth(), 0).getDay(); i++) {
             const notThisMonthDay = document.createElement('div');
             notThisMonthDay.classList.add('notThisMonthDay');
 
@@ -77,9 +149,16 @@ class Scheduler {
         }
         for(let i = 0; i < new Date(dateObject.getFullYear(), dateObject.getMonth() + 1, 0).getDate(); i++) {
             const day = document.createElement('div');
-            day.classList.add('currentMonthDay');
+            day.classList.add('currentShownMonthDay');
+            day.innerText = (i + 1).toString();
 
             calendarPage.appendChild(day);
+        }
+        for(let i = 0; i < calendarPage.children.length % 7; i++) {
+            const notThisMonthDay = document.createElement('div');
+            notThisMonthDay.classList.add('notThisMonthDay');
+
+            calendarPage.appendChild(notThisMonthDay);
         }
 
         return calendarPage;

9121c44a3769172a9310ab011935b52e4c0a271f
David Voskanyan <voskanyandavid001001@gmail.com>
2023-04-20 01:17:39 +0400

    Analytics page is connected to backend

--- front/assets/globalScripts/NumbersAnimator.js
+++ front/assets/globalScripts/NumbersAnimator.js
@@ -1,7 +1,7 @@
 class NumbersAnimator {
     constructor() {
     }
-    increaseAnimate(startValue, finalValue, DomElement) {
+    static increaseAnimate(startValue, finalValue, DomElement) {
         let currentValue = startValue;
         const increaseInterval = setInterval(() => {
             if(currentValue === finalValue) { clearInterval(increaseInterval); }
